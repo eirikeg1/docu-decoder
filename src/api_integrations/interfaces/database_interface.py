9d51aec069d21265b622b9e804cc5f41e2f83c0b
eirik e <e.eggset@gmail.com>
2024-01-03 00:16:42 +0100

    create database interface

--- src/api_integrations/interfaces/database_interface.py
+++ src/api_integrations/interfaces/database_interface.py
@@ -3,5 +3,19 @@ from abc import abstractmethod
 class DatabaseInterface:
     
     @abstractmethod
-    def add_entry(self, entry: dict, key: str = None):
+    def add_document(self, document: dict) -> bool:
+        """Adds a single document to the database
+
+        Args:
+            document (dict): {id: str?, title: str, paragraphs: List[str]?, course_id: str, author_name: str?}
+            
+        Returns:
+            bool: True if the document was added successfully, False otherwise
+        """
         pass
+    
+    @abstractmethod
+    def add_paragraph(self, paragraph: dict):
+        pass
+    
+    
--- src/api_integrations/interfaces/supabase.py
+++ src/api_integrations/interfaces/supabase.py
@@ -1,10 +1,17 @@
 import os
 from supabase import create_client, Client
+from src.api_integrations.interfaces.database_interface import DataBaseInterface
 
-class SupabaseInterface:
+class SupabaseInterface(DataBaseInterface):
     
     def __init__(self):
         url: str = os.environ.get("SUPABASE_URL")
         key: str = os.environ.get("SUPABASE_KEY")
 
-        supabase: Client = create_client(url, key)
+        self.supabase: Client = create_client(url, key)
+        
+    def add_document(self, document: dict) -> bool:
+        pass
+    
+    def add_paragraph(self, paragraph: dict):
+        pass
